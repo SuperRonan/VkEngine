12e92244c8d2dcb64910ab1f44cc413901f75651
Ronan Cailleau <ro.cailleau@gmail.com>
2025-03-04 11:34:20 +0100

    [misc] Added Ray.slang + extra

--- Shaders/ShaderLib/Rendering/Ray.glsl
+++ Shaders/ShaderLib/Rendering/Ray.glsl
@@ -4,20 +4,20 @@
 
 struct Ray
 {
-    vec3 origin;
-    vec3 direction;
+	vec3 origin;
+	vec3 direction;
 };
 
 vec3 sampleRay(const in Ray r, float t)
 {
-    return r.origin + t * r.direction;
+	return r.origin + t * r.direction;
 }
 
 Ray TransformAssumeRigid(const in mat4x3 xform, Ray ray)
 {
-    ray.origin = xform * vec4(ray.origin, 1);
-    ray.direction = mat3(xform) * ray.direction;
-    return ray;
+	ray.origin = xform * vec4(ray.origin, 1);
+	ray.direction = mat3(xform) * ray.direction;
+	return ray;
 }
 
 
