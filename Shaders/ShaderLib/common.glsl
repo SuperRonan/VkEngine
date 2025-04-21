4af4a056dc9d11213242f051d26ecfeaef084e54
Ronan Cailleau <ro.cailleau@gmail.com>
2025-04-21 17:12:15 +0200

    [typo] common.glsl

--- Shaders/ShaderLib/common.glsl
+++ Shaders/ShaderLib/common.glsl
@@ -3,8 +3,8 @@
 #include "core"
 #include "bindings"
 
- layout(row_major) uniform;
- layout(row_major) buffer;
+layout(row_major) uniform;
+layout(row_major) buffer;
 
 #define NonUniformEXT(X) nonuniformEXT(X)
 
