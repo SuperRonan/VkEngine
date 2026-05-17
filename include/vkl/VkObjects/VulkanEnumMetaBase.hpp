d348b5fc6fc0d1f2bee271b758b461ea570b12a0
Ronan Cailleau <ro.cailleau@gmail.com>
2026-05-17 16:48:48 +0200

    [misc] Improve generator scripts `common.py`
    - Add common `CONSTINIT / CONSTEVAL` macro helpers
    - Add `CommonBaseGenerator` helper class
    - Add separate source/header generation paths
    - Add default `#pragma once` / "file is generated comment" argument in `CStyleWriter`
    - Fix erroneous parant `__init__()` with `super()`

--- include/vkl/VkObjects/VulkanEnumMetaBase.hpp
+++ include/vkl/VkObjects/VulkanEnumMetaBase.hpp
@@ -8,11 +8,11 @@ namespace vku
 	template <that::concepts::Enumeration Enum>
 	struct EnumMetaInfo
 	{
-		static constexpr const bool IsBitField = false;
-		static constexpr const unsigned int UniqueId = unsigned(-1);
+		static constinit const bool IsBitField = false;
+		static constinit const unsigned int UniqueId = unsigned(-1);
 		using EnumType = Enum;
-		static constexpr const char* Name = nullptr;
-		static constexpr const char* ValuePrefix = nullptr;
+		static constexpr const char* const Name = nullptr;
+		static constexpr const char* const ValuePrefix = nullptr;
 		static constexpr const char* GetValueName(Enum value) {
 			return nullptr;
 		}
